88b8ed223dd17804e91be7650bd792c0b1f20094
BanJung <higkdl813@gmail.com>
2023-12-11 09:41:13 +0900

    12.11 introduction

--- src/main/java/hyundai/cc/lecturemanage/lecture/service/LectureService.java
+++ src/main/java/hyundai/cc/lecturemanage/lecture/service/LectureService.java
@@ -1,16 +1,21 @@
 package hyundai.cc.lecturemanage.lecture.service;
 
 
+import hyundai.cc.domain.Criteria;
 import hyundai.cc.lecturemanage.lecture.dto.LectureCreateDTO;
 import hyundai.cc.lecturemanage.lecture.dto.LectureDTO;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface LectureService {
     public LectureDTO createLecture(LectureCreateDTO lec);
-    public ArrayList<LectureDTO> getLectureList();
+    public List<LectureDTO> getLectureList();
     public LectureDTO getLectureDetail(Long lectureId);
     public LectureDTO updateLecture(Long lectureId, LectureCreateDTO lec);
     public String deleteLecture(Long lectureId);
+    public int getTotal(Criteria cri);
+    public List<LectureDTO> getLecturesByPage(Criteria cri);
+
+
 
 }
--- src/main/java/hyundai/cc/lecturemanage/lecture/service/MockLectureServiceImpl.java
+++ src/main/java/hyundai/cc/lecturemanage/lecture/service/MockLectureServiceImpl.java
@@ -1,41 +1,61 @@
-package hyundai.cc.lecturemanage.lecture.service;
-
-import hyundai.cc.lecturemanage.lecture.dto.LectureCreateDTO;
-import hyundai.cc.lecturemanage.lecture.dto.LectureDTO;
-import hyundai.cc.lecturemanage.lecture.mapper.LectureMapper;
-import lombok.extern.java.Log;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-@Log
-@Service
-public class MockLectureServiceImpl implements LectureService{
-
-    @Override
-    public LectureDTO createLecture(LectureCreateDTO lec) {
-        return null;
-    }
-
-    @Override
-    public ArrayList<LectureDTO> getLectureList() {
-        return null;
-    }
-
-    @Override
-    public LectureDTO getLectureDetail(Long lectureId) {
-        return null;
-    }
-
-    @Override
-    public LectureDTO updateLecture(Long lectureId, LectureCreateDTO lec) {
-        return null;
-    }
-
-    @Override
-    public String deleteLecture(Long lectureId) {
-        return null;
-    }
-}
+//package hyundai.cc.lecturemanage.lecture.service;
+//
+//import hyundai.cc.domain.Criteria;
+//import hyundai.cc.lecturemanage.lecture.dto.LectureCreateDTO;
+//import hyundai.cc.lecturemanage.lecture.dto.LectureDTO;
+//import hyundai.cc.lecturemanage.lecture.dto.LectureDTOMapper;
+//import hyundai.cc.lecturemanage.lecture.mapper.LectureMapper;
+//import lombok.extern.java.Log;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Log
+//@Service
+//public class MockLectureServiceImpl implements LectureService{
+//
+//    private final LectureMapper lectureMapper;
+//
+//    private final LectureDTOMapper lectureDTOMapper;
+//    @Autowired
+//    public MockLectureServiceImpl(LectureMapper lectureMapper,LectureDTOMapper lectureDTOMapper) {
+//        this.lectureMapper = lectureMapper; this.lectureDTOMapper=lectureDTOMapper;
+//    }
+//
+//    @Override
+//    public LectureDTO createLecture(LectureCreateDTO lec) {
+//        return null;
+//    }
+//
+//    @Override
+//    public ArrayList<LectureDTO> getLectureList() {
+//        return null;
+//    }
+//
+//    @Override
+//    public LectureDTO getLectureDetail(Long lectureId) {
+//        return null;
+//    }
+//
+//    @Override
+//    public LectureDTO updateLecture(Long lectureId, LectureCreateDTO lec) {
+//        return null;
+//    }
+//
+//    @Override
+//    public String deleteLecture(Long lectureId) {
+//        return null;
+//    }
+//
+//    @Override
+//    public int getTotal(Criteria cri) {
+//        return lectureMapper.getTotalCount(cri);
+//    }
+//
+//    @Override
+//    public List<LectureDTO> getLecturesByPage(Criteria cri) {
+//        return lectureMapper.getLecturesByPage(cri);
+//    }
+//}
